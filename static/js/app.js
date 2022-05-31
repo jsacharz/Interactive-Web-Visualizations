b733659b37cf745a952018ae1a9957b869302cb0
jsacharz <98008369+jsacharz@users.noreply.github.com>
2022-05-31 14:53:28 +1000

    final

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,10 @@
 d3.json("samples.json").then(data => console.log(data))
 var selector = d3.select("#selDataset")
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-//   * Use `sample_values` as the values for the bar chart.
-
-//   * Use `otu_ids` as the labels for the bar chart.
-
-//   * Use `otu_labels` as the hovertext for the chart.
-
-//   ![bar Chart](Images/hw01.png)
+// Build function for Charts:
 function buildChart() {
 
+// Retrive json data
   d3.json("samples.json").then(data => {
    
 
@@ -23,7 +17,7 @@ function buildChart() {
     var sampleData = data.samples.filter(sample => sample.id == selected_sample)
  
 
-    // BAR CHART
+//2. HORIZONTAL BAR CHART to display top 10 OTUs from individuals:
     trace1 = {
         x: sampleData[0].sample_values.slice(0,10),
         y: sampleData[0].otu_ids.slice(0,10).map(id => 'OTU' + id),
@@ -61,7 +55,7 @@ function buildChart() {
       // Display bubble chart
       Plotly.newPlot('bubble', bubbleData, layout);
 
-    // 4. Display the sample metadata, i.e., an individual's demographic information
+// 4. Display the sample metadata, i.e., an individual's demographic information
     
     var demogrData = data.metadata.filter(row => row.id == selected_sample)
     console.log(demogrData);
@@ -80,27 +74,3 @@ function buildChart() {
 buildChart();
 
 document.getElementById("selDataset").addEventListener("change", buildChart)
-
-
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-// ![hw](Images/hw03.png)
-
-// 6. Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown below:
-
-// ![hw](Images/hw02.png)
-
-// 7. Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo. Ensure that your repository has regular commits and a thorough README.md file
-
-// ## Advanced Challenge Assignment (Optional)
-
-// The following task is advanced and therefore optional.
-
-// * Adapt the Gauge Chart from <https://plot.ly/javascript/gauge-charts/> to plot the weekly washing frequency of the individual.
-
-// * You will need to modify the example gauge code to account for values ranging from 0 through 9.
-
-// * Update the chart whenever a new sample is selected.
-
-// ![Weekly Washing Frequency Gauge](Images/gauge.png)
